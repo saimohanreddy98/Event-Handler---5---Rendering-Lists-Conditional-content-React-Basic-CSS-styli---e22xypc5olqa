6eec1c58f587be2f12f25a91b0f807ac85561659
saimohanreddy98 <saimohanreddy98@gmail.com>
2022-12-21 19:02:09 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,23 @@
 import React from 'react'
 import '../styles/App.css';
 const App = () => {
+  const[data,setData]=useState("");
+const handleChange = (event)=> { 
+setData(event.target.value)
+
+}
+ const handleSubmit =(event) => {
+  event.preventDefault();
+  console.log("form submitted")
+ }
+
 
 
   return (
     <div id="main">
       <form>
         <label htmlFor='name'>Name</label>
-        <input id="name" type={"text"} />
+        <input id="name" type={"text"}  value ={ data } onChange = {handleChange}/>
 
         <br />
         <br />
